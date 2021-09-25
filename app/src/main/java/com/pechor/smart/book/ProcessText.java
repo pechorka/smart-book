1955650e1d01cdd8c3808c1cc2fd73427746da61
pechorka <pechor44@gmail.com>
2021-09-26 00:08:05 +0300

    refactor to prepare for addition of image dialog

--- app/src/main/java/com/pechor/smart/book/ProcessText.java
+++ app/src/main/java/com/pechor/smart/book/ProcessText.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class ProcessText extends AppCompatActivity {
 
@@ -14,14 +13,14 @@ public class ProcessText extends AppCompatActivity {
         CharSequence text = getIntent()
                 .getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
 
-        Intent intent = new Intent(this, DialogActivity.class);
+        Intent intent = new Intent(this, TextDialogActivity.class);
 // NEW_TASK allows the new dialog activity to be separate from the existing activity.
 // REORDER_TO_FRONT causes the dialog activity to be moved to the front,
 // if it's already running.
 // Without the NEW_TASK flag, the existing "base" activity
 // would be moved to the front as well.
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra(DialogActivity.TEXT_TO_PROCESS_PARAM, text.toString());
+        intent.putExtra(TextDialogActivity.TEXT_TO_PROCESS_PARAM, text.toString());
 // The activity must be started from the application context.
 // I'm not sure why exactly.
         this.getApplicationContext().startActivity(intent);
--- app/src/main/java/com/pechor/smart/book/TextDialogActivity.java
+++ app/src/main/java/com/pechor/smart/book/TextDialogActivity.java
@@ -2,21 +2,13 @@ package com.pechor.smart.book;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-public class DialogActivity extends Activity {
+public class TextDialogActivity extends Activity {
     public static final String TEXT_TO_PROCESS_PARAM = "TEXT_TO_PROCESS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +23,7 @@ public class DialogActivity extends Activity {
 
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.activity_process_text);
+        dialog.setContentView(R.layout.webview);
 
         WebView wv =  dialog.findViewById(R.id.webview);
 
@@ -48,7 +40,7 @@ public class DialogActivity extends Activity {
         dialog.setCanceledOnTouchOutside(true);
         dialog.setCancelable(true);
 
-        dialog.setOnDismissListener(dialogInterface -> DialogActivity.this.finish());
+        dialog.setOnDismissListener(dialogInterface -> TextDialogActivity.this.finish());
 
         dialog.show();
     }
